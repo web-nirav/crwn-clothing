b06115cf3cddc0114e081be6a7a1201aa5e4b037
nirav.prajapati <nirav.prajapati@indianic.com>
2019-08-06 14:37:50 +0530

    70-71 Header component adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
-import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <HomePage />
-      </div>
-    );
-  }
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./components/shop/shop.component";
+import Header from "./components/header/header.component";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/shop/shop.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import SHOP_DATA from "./shop.data.js";
+import CollectionPreview from "../collection-preview/collection-preview";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+
+  // here we need to think about performance impact as we are runnig loop and inside it we are rendering component so whenever ShopPage component re-render all the loop component will also re-render so make note for future...
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
